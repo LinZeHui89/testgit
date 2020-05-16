bf38cc171618afcf05213725c0f53f5b0ad8ca07
lzh <my@qq.com>
2020-05-16 12:51:49 +0800

    fetch-lzh

--- src/fetch/fetchMain.java
+++ src/fetch/fetchMain.java
@@ -3,5 +3,7 @@ package fetch;
 public class fetchMain {
     public static void main(String[] args) {
         System.out.println("fetch");
+        System.out.println("fetchlzh add");
+
     }
 }
---
+++ src/fetch/newFetch.java
@@ -0,0 +1,7 @@
+package fetch;
+
+public class newFetch {
+    public static void main(String[] args) {
+        System.out.println("fetchlzh new ");
+    }
+}
