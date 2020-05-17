2c1c3fc1a6fff20097bd8b54758323fb16132431
lzh <my@qq.com>
2020-05-17 10:44:37 +0800

    fetch-lzh

--- src/fetch/fetchMain.java
+++ src/fetch/fetchMain.java
@@ -1,6 +1,6 @@
 package fetch;
 
-public class fetchMain {
+public class  fetchMain {
     public static void main(String[] args) {
         System.out.println("fetch");
         System.out.println("fetch add something");
